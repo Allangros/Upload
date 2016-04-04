503ad85e21ec0014b9ba489657b8f84f69c1503b
allangros <allanipod@outlook.fr>
2016-04-04 21:37:49 +0200

    up

--- Codility/src/Codility/CodilityProfit.java
+++ Codility/src/Codility/CodilityProfit.java
@@ -1,17 +1,34 @@
 package Codility;
 
+import java.util.ArrayList;
+
 public class CodilityProfit {
 
 	public static void main(String[] args) {
-CodilityProfit pro=new CodilityProfit();
-pro.init();
+		CodilityProfit pro = new CodilityProfit();
+		pro.init();
 	}
-	public void init(){
-		int[]A={23171,21011,21123,21366,21013,21367};
-		int Valmax=0;
-		for (int i = 0; i < A.length; i++) {
-			
+
+	public void init() {
+		int[] A = { 23171, 21011, 21123, 21366, 21013, 21367 };
+		ArrayList intList = new ArrayList<Integer>();
+		for (int intValue : A) {
+			intList.add(intValue);
 		}
+		// ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(A));
+		// Integer[] B = A.clone();
+		int cpt = 0;
+		int Valmax = 0;
+		for (int i = 0; i < A.length - 1; i++) {
+			cpt++;
+			int lol = (int) intList.get(cpt)- A[i];
+			Valmax = Math.max(Valmax, lol);
+
+			// for (int j = i + 1; j < A.length; j++) {
+			// Valmax = (int) Math.max(Valmax, A[j] - A[i]);
+			// }
+		}
+		System.out.println(Valmax);
 	}
 
 }
